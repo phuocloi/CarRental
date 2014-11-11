a795f26a069122eeb7e6ae03db111a260c554675
k-earn9x <k.earn9x@gmail.com>
2014-11-11 13:10:22 +0700

    add service reference

--- CarRentalWebClient/CarRentalWebClient/Controllers/DefaultController.cs
+++ CarRentalWebClient/CarRentalWebClient/Controllers/DefaultController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
 
 namespace CarRentalWebClient.Controllers
 {
-    public class DefaultController : Controller
+    public class DefaultController : SiteController
     {
         // GET: Default
         public ActionResult Index()
         {
-            CarRentalServiceReference.DbContextModel db = new CarRentalServiceReference.DbContextModel();
-            ViewBag.Models = db.CarModels.
+            var items = Db.CarModels.ToList();
+            List<CarRentalServiceReference.CarModel> results = new List<CarRentalServiceReference.CarModel>();
+
+            foreach (var item in items)
+            {
+                item.Brand = Db.CarBrands.Where(b => b.Id == item.Brand_Id).Single();
+                results.Add(item);
+            }
+
+            ViewBag.Models = results;
             return View();
         }
     }
---
+++ CarRentalWebClient/CarRentalWebClient/Controllers/SiteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+
+namespace CarRentalWebClient.Controllers
+{
+    public class SiteController : Controller
+    {
+        private Uri Root = new Uri("http://localhost:1499/Services/DataService.svc");
+        public CarRentalServiceReference.DbContextModel Db = new CarRentalServiceReference.DbContextModel(new Uri("http://localhost:1499/Services/DataService.svc"));
+    }
+}
