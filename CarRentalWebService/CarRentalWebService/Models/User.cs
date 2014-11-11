272f60a7f94c9e19193661379baa28b09fd64e61
Nguyen Hoang Nam <hoangnam19922010@gmail.com>
2014-11-11 22:55:38 +0700

    Add Login

---
+++ CarRentalWebService/CarRentalWebService/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalWebService.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
